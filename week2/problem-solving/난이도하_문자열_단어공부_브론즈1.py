b4e73443b7e3cbef86ad20d7922fed1470c6994b
고명석 <kms86989408@gmail.com>
2026-03-12 23:46:51 +0900

    solve : baekjoon 1157

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,16 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+spelling = input().upper()
+lot = ""
+max_count = 0
+
+for i in set(spelling):
+    cnt = spelling.count(i)
+    if cnt > max_count:
+        max_count = cnt
+        lot = i
+    elif cnt == max_count:
+        lot = "?"
+
+print(lot)
