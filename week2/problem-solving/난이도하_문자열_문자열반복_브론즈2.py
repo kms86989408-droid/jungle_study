9dcf794557358203ea73460290d290d7eec37c31
고명석 <kms86989408@gmail.com>
2026-03-12 22:33:15 +0900

    solve : 2675

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,15 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+n = int(input())
+
+for i in range(n):
+    R, S = input().split()
+    R = int(R)
+    
+    result = ""
+
+    for j in S:
+        result += j * R
+    print(result)
+
