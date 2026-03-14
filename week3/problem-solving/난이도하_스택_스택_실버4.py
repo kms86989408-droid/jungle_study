58fa8be9ccb3db5d47306fc598d5b64249a0cdf6
고명석 <kms86989408@gmail.com>
2026-03-14 19:51:37 +0900

    solve : baekjoon 10828

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,36 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+result = []
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == "push":
+        stack.append(command[1])
+
+    elif command[0] == "pop":
+        if len(stack) == 0:
+            result.append("-1")
+        else:
+            result.append(stack.pop())
+
+    elif command[0] == "size":
+        result.append(str(len(stack)))
+
+    elif command[0] == "empty":
+        if len(stack) == 0:
+            result.append("1")
+        else:
+            result.append("0")
+
+    elif command[0] == "top":
+        if len(stack) == 0:
+            result.append("-1")
+        else:
+            result.append(stack[-1])
+
+print("\n".join(result))
