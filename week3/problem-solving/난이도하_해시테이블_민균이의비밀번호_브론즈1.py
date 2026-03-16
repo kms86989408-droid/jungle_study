7421ccb1a0c235c03d12a3d79e825d933b80e6ba
고명석 <kms86989408@gmail.com>
2026-03-16 11:40:54 +0900

    solve : baekjoon 9933

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,22 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+
+n = int(input())
+words = []
+for i in range(n):
+    words.append(input())
+for word in words:
+    reverse_word = word[::-1]
+
+    if reverse_word in words:
+        num_result = len(word)//2
+        result = word[num_result]
+        print(len(word), result)
+        break
+
+
+
+
+
+
+
